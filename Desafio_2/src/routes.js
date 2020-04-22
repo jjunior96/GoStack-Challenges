0a46768ca3291aa528b013f9aff08b23bd41b6c9
Junior Alves <jjalves96@gmail.com>
2020-04-22 08:49:44 -0300

    feat: Recipient Create is private route

--- Desafio_2/src/routes.js
+++ Desafio_2/src/routes.js
@@ -4,12 +4,16 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionController.store);
 
+// private routes
+routes.use(authMiddleware);
 routes.post('/recipients', RecipientController.store);
 
 export default routes;

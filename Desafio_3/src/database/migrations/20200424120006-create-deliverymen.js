3842f632e8322171e2051efc0027e7ce68a0efd4
Junior Alves <jjalves96@gmail.com>
2020-04-24 09:09:48 -0300

    feat: Deliveryman migration create

---
+++ Desafio_3/src/database/migrations/20200424120006-create-deliverymen.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('deliverymen', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primary: true,
+        allowNull: false,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      avatar_id: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('deliverymen');
+  },
+};
